185b45f553ae0d82c1d9e30101857f603137756d
boletales <30789006+boletales@users.noreply.github.com>
2024-02-27 11:47:22 +0900

    fix symlink.py

--- symlink.py
+++ symlink.py
@@ -2,10 +2,10 @@
 import glob, os, os.path, itertools
 
 os.chdir(os.path.dirname(os.path.abspath(__file__))+"/files")
-for cd, dirs, files in os.walk("./"):
-    for f in itertools.chain(dirs, files):
-        print(os.path.abspath(f))
-        dest = os.path.expanduser("~")+(cd[1:]+"/"+f).replace("//","/")
+with os.scandir("./") as entries:
+    for entry in entries:
+        print(os.path.abspath(entry.name))
+        dest = os.path.expanduser("~")+(cd[1:]+"/"+entry.name).replace("//","/")
         if not os.path.exists(dest):
-            os.symlink(os.path.abspath(f), dest)
+            os.symlink(os.path.abspath(entry.name), dest)
             print("created: "+dest)
