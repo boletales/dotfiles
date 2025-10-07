7266ec60501f2ffdd9866efadaa288ad4905524c
boletales <30789006+boletales@users.noreply.github.com>
2025-10-07 11:14:35 +0900

    init.lua 追記

--- files/.config/nvim/init.lua
+++ files/.config/nvim/init.lua
@@ -14,6 +14,7 @@ vim.opt.number = true
 vim.opt.relativenumber = false
 vim.opt.cursorline = true
 
+
 vim.opt.smartindent = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
@@ -21,3 +22,15 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 
 vim.opt.wrap = false
+
+vim.opt.mouse = 'a'
+vim.opt.scrolloff = 8
+
+vim.opt.signcolumn = 'yes'
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- shortcuts
+vim.keymap.set('n', '<ESC><ESC>', ':nohlsearch<CR>', { silent = true })
+
