5517e95896c334ad164dcff694c6f356707cf035
boletales <30789006+boletales@users.noreply.github.com>
2025-10-07 14:51:04 +0900

    不要な記述を削除

--- files/.config/nvim/init.lua
+++ files/.config/nvim/init.lua
@@ -1,7 +1,5 @@
 
 vim.scriptencoding = 'utf-8'
-vim.opt.encoding = 'utf-8'
-vim.opt.fileencoding = 'utf-8'
 vim.opt.fileencodings = 'utf-8,sjis'
 
 vim.opt.backup = false
@@ -14,7 +12,6 @@ vim.opt.number = true
 vim.opt.relativenumber = false
 vim.opt.cursorline = true
 
-
 vim.opt.smartindent = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
@@ -23,13 +20,7 @@ vim.opt.softtabstop = 4
 
 vim.opt.wrap = false
 
-vim.opt.mouse = 'a'
-vim.opt.scrolloff = 8
-
 vim.opt.signcolumn = 'yes'
-
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
 
 -- shortcuts
 vim.keymap.set('n', '<ESC><ESC>', ':nohlsearch<CR>', { silent = true })
